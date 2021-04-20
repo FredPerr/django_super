77f626c1e14badc4c8e350fdea76097bd68bed9a
Frederic Perron <frederic.norrep@gmail.com>
2021-04-20 19:04:44 -0400

    added the script support

--- super/views.py
+++ super/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'super/home.html', context={'scss_filename': 'home.scss'})
+    return render(request, 'super/home.html', context={'scss_filename': 'home.scss', 'js_filename': 'home.js'})
