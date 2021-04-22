68960ce6e44c8b302fd4ab07ded5f95dcd0552d2
Frederic Perron <frederic.norrep@gmail.com>
2021-04-22 18:58:00 -0400

    added the range template tag

--- super/templatetags/range.py
+++ super/templatetags/range.py
@@ -1,3 +1,12 @@
-# from django import template
+from django import template
+from builtins import range as __range__
 
-# register = template.Library()
+
+register = template.Library()
+
+@register.filter(name='range')
+def range(value, zero_index=True):
+    if not zero_index:
+        return __range__(1, value, 1) 
+    return __range__(value)
+
