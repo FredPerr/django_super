5463e75259beea9b57404fc5e93b792749c3c26a
Frederic Perron <frederic.norrep@gmail.com>
2021-04-29 17:59:31 -0400

    added the unimplemented supervisor script

---
+++ supervisor.py
@@ -0,0 +1,23 @@
+def _search_and_replace():
+    pass
+
+def _get_project_name():
+    pass
+
+def use_scss():
+    pass
+
+def use_typescript():
+    pass
+
+def rename_project():
+    pass
+
+def help():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
