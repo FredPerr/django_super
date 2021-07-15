3d2a0453a75650dbc5a326f826a761da1c8394ee
Frederic Perron <frederic.norrep@gmail.com>
2021-07-14 22:47:16 -0400

    added form styling and manual overridding

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,15 @@ from django import forms
 from django.contrib.auth import get_user_model
 
 
+INPUT_ATTRIBUTES = {'placeholder': ' ', 'class': 'input'}
+
+
 class RegisterForm(UserCreationForm):
+
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
+    email = forms.CharField(label='Email', widget=forms.EmailInput(attrs=INPUT_ATTRIBUTES))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(INPUT_ATTRIBUTES, **{'autocomplete': 'new-password'})))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(INPUT_ATTRIBUTES, **{'autocomplete': 'new-password'})))
 
     class Meta:
         model = get_user_model()
@@ -11,8 +19,13 @@ class RegisterForm(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label='Email or username')
+    username = forms.CharField(label='Email or username', widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
+    password = forms.CharField(widget=forms.PasswordInput(attrs=INPUT_ATTRIBUTES))
 
+    error_messages = {
+        'invalid_login': "Please enter the correct %(username)s and password.",
+        'inactive': "This account is inactive."
+    }
 
 class PasswordChange(PasswordChangeForm):
     pass
