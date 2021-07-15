803c677a68d9d2a4e9db69f770ce27490400e8c2
Frederic Perron <frederic.norrep@gmail.com>
2021-07-15 15:33:34 -0400

    added the final style of the accounts app

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,8 +10,8 @@ class RegisterForm(UserCreationForm):
 
     username = forms.CharField(label='Username', widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
     email = forms.CharField(label='Email', widget=forms.EmailInput(attrs=INPUT_ATTRIBUTES))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(INPUT_ATTRIBUTES, **{'autocomplete': 'new-password'})))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(INPUT_ATTRIBUTES, **{'autocomplete': 'new-password'})))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', **INPUT_ATTRIBUTES}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', **INPUT_ATTRIBUTES}))
 
     class Meta:
         model = get_user_model()
@@ -27,27 +27,44 @@ class LoginForm(AuthenticationForm):
         'inactive': "This account is inactive."
     }
 
-class PasswordChange(PasswordChangeForm):
-    pass
-
-
-class PasswordResetForm(PasswordResetForm):
-    email = forms.EmailField(
-        label="Email",
-        max_length=254,
-        widget=forms.EmailInput(attrs=dict({'autocomplete': 'email'}, **INPUT_ATTRIBUTES)
-    ))
-
-
-class PasswordResetConfirmForm(SetPasswordForm):
+class PasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        label= "Old password", strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'autofocus': True, **INPUT_ATTRIBUTES}),
+    )
     new_password1 = forms.CharField(
         label= "New password",
-        widget=forms.PasswordInput(attrs=dict({'autocomplete': 'new-password'}, **INPUT_ATTRIBUTES)),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', **INPUT_ATTRIBUTES}),
         strip=False,
         help_text=password_validation.password_validators_help_text_html(),
     )
     new_password2 = forms.CharField(
         label= "New password confirmation",
         strip=False,
-        widget=forms.PasswordInput(attrs=dict({'autocomplete': 'new-password'}, **INPUT_ATTRIBUTES)),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', **INPUT_ATTRIBUTES}),
+    )
+    error_messages = {
+        'password_incorrect': "Your old password was entered incorrectly.",
+    }
+
+
+class PasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(
+        label="Email",
+        max_length=254,
+        widget=forms.EmailInput(attrs={'autocomplete': 'email', **INPUT_ATTRIBUTES})
+    )
+
+
+class PasswordResetConfirmForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label= "New password",
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', **INPUT_ATTRIBUTES}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label= "New password confirmation",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', **INPUT_ATTRIBUTES}),
     )
