5878a378867564fa80d22b794d31e3fdc7ddcfd4
Frederic Perron <frederic.norrep@gmail.com>
2021-07-30 11:34:49 -0400

    removed unused signals file

--- accounts/signals.py
+++
@@ -1,8 +0,0 @@
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from .models import Account
-
-
-# # @receiver(post_save, sender=Account)
-# # def signal_registered_successful(sender, **kwargs):
-# #     pass
