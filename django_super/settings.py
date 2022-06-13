9b519b10ff0783daba70f37a4f8965e276a2ca14
Frederic Perron <frederic.norrep@gmail.com>
2022-06-13 16:16:30 -0400

    added the media url

--- django_super/settings.py
+++ django_super/settings.py
@@ -159,6 +159,7 @@ EMAIL_USE_TLS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
